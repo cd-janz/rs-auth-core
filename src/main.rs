c36f71009dfc673db699a04dd3afa56c87e5a9d0
cd-janz <cd.juanzuniga@gmail.com>
2026-01-29 21:28:57 -0500

    swagger UI added

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,32 @@ use axum::{
     Router,
 };
 use tonic::transport::Server;
+use utoipa::OpenApi;
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_axum::routes;
+use utoipa_swagger_ui::SwaggerUi;
 use crate::infrastructure::infra_state::InfraState;
 use crate::presentation::grpc::pb::user::user_service_server::UserServiceServer;
 use crate::presentation::grpc::user_service_handler::UserHandler;
 
+#[derive(OpenApi)]
+#[openapi(
+    paths(hello),
+)]
+struct ApiDoc;
+
 #[tokio::main]
 async fn main() {
     // STATES
-    let _infra = InfraState::new().await;
+    let infra = InfraState::new().await;
     // AXUM REST Config
     let rest_addr: SocketAddr = "0.0.0.0:8080".parse().expect("couldn't parse rest address");
-    let api = Router::new().route("/api/v1", get(hello));
+    let (_, api_spec) = OpenApiRouter::<InfraState>::with_openapi(ApiDoc::openapi())
+        .routes(routes!(hello))
+        .split_for_parts();
+    let api = Router::new().route("/api/v1", get(hello))
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api_spec))
+        .with_state(infra);
     let api_ltr = tokio::net::TcpListener::bind(rest_addr)
         .await.expect("couldn't start axum tcp server");
     // Axum-Tonic GRPC Config
@@ -31,6 +46,7 @@ async fn main() {
     );
 }
 
+#[utoipa::path(get, path="/api/v1", responses((status = 200, description = "Hello World", body = &str)))]
 async fn hello() -> &'static str {
     "Hello, World!"
 }
